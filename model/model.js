5b9937b0ccce77b0f2e72ee3a97633809093517f
Ritwikgotbugs <shritwik04@gmail.com>
2024-01-27 16:23:40 +0530

    Initialise mongoose and connect to database

---
+++ model/model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.Database_URL)
+    .then(() => console.log('Connected to Database'))
+    .catch(error => console.error('Error connecting to Database:', error));
+
+// Define Mongoose schema and model
+const climateSchema = new mongoose.Schema({
+    location: { type: String, required: true },
+    temperature: { type: Number, required: true },
+    humidity: { type: Number, required: true },
+    wind: { type: Number, required: true },
+    uvindex: { type: Number, required: true },
+    precipitation: { type: Number, required: true },
+    weatherIcon: { type: String, required: true },
+}, { timestamps: true });
+
+const Climate = mongoose.model('Climate', climateSchema);
+
+module.exports = Climate;
--- server.js
+++ server.js
@@ -1,17 +1,49 @@
 const express = require('express');
-const mongooose = require('mongoose');
-const app = express();
+const bodyParser = require('body-parser');
 const path = require('path');
-const port = 3000;
+require('dotenv').config();
+const Climate = require('./model/model.js');
 
+const app = express();
+const port = 5500;
+
+// Middleware setup
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/views/index.html'));
+
+
+// Route to save weather data
+app.post('/save-weather-data', (req, res) => {
+    const weatherData = req.body;
+
+    const newWeatherData = new Climate({
+        location: weatherData.location.name,
+        country: weatherData.location.country,
+        temperature: weatherData.current.temp_c,
+        humidity: weatherData.current.humidity,
+        wind: weatherData.current.wind_kph,
+        uvindex: weatherData.current.uv,
+        precipitation: weatherData.current.precip_mm,
+        weatherIcon: weatherData.current.condition.icon
+    });
+    newWeatherData.save()
+        .then(savedData => res.json(savedData))
+        .catch(err => res.status(500).json({ error: err.message }));
 });
 
-app.get('/history', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/views/history.html'));
+//Route to fetch weather data
+app.get('/fetch-history', (req, res) => {
+    Climate.find()
+        .then(data => res.json(data))
+        .catch(err => res.status(500).json({ error: err.message }));
 });
 
-app.listen(port, () => console.log(`http://localhost:${port}`));
+
+
+// Routes for HTML pages
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/views/index.html')));
+app.get('/history', (req, res) => res.sendFile(path.join(__dirname, '/public/views/history.html')));
+
+// Start server
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
